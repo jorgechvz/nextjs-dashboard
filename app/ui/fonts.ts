e091e825734e162e6b81303e00f01e378899b012
jorgechvz <jorgechvzpon@gmail.com>
2023-11-08 12:51:29 -0500

    nextjs dashboard until Chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+/* Export Inter Font */
+export const inter = Inter({ subsets: ["latin"] });
+
+/* Export Montserrat font */
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "700" });
